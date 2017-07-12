9d28a8ec974996edac76aded6171c1fac5f1a016
MathF <mathias.fillon@gmail.com>
2017-07-12 18:02:27 +0200

    derniers push

--- public/js/components/blog/blogItem/blogItem.js
+++ public/js/components/blog/blogItem/blogItem.js
@@ -98,9 +98,10 @@ let blogItem = {
                     return post_id !== this.post._id
                 })
             }
+            
 
             UsersService.update(this.user).then((res) => {
-                //return UsersService.setToken(res.data.token)
+                // return UsersService.setToken(res.data.token)
             }).then((user) => {
                 Materialize.toast((postFound ? 'Removed' : 'Added'), 2000, (postFound ? 'toast-warning' : 'toast-success'))
             }).catch((err) => {
@@ -109,37 +110,37 @@ let blogItem = {
             })
         }
 
-    //     var currentTime = new Date();
-    //    this.currentTime = currentTime;
-    //    this.month = ['Januar', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    //    this.monthShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    //    this.weekdaysFull = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    //    this.weekdaysLetter = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-    //    this.disable = [false, 1, 7];
-    //    this.today = 'Today';
-    //    this.clear = 'Clear';
-    //    this.close = 'Close';
-    //     var days = 15;
-    //    this.minDate = (new Date(this.currentTime.getTime() - (1000 * 60 * 60 * 24 * days))).toISOString();
-    //    this.maxDate = (new Date(this.currentTime.getTime() + (1000 * 60 * 60 * 24 * days))).toISOString();
-    //    this.onStart = function () {
-    //         console.log('onStart');
-    //     };
-    //    this.onRender = function () {
-    //         console.log('onRender');
-    //     };
-    //    this.onOpen = function () {
-    //         console.log('onOpen');
-    //     };
-    //    this.onClose = function () {
-    //         console.log('onClose');
-    //     };
-    //    this.onSet = function () {
-    //         console.log('onSet');
-    //     };
-    //    this.onStop = function () {
-    //         console.log('onStop');
-    //     };
+        //     var currentTime = new Date();
+        //    this.currentTime = currentTime;
+        //    this.month = ['Januar', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        //    this.monthShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        //    this.weekdaysFull = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        //    this.weekdaysLetter = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+        //    this.disable = [false, 1, 7];
+        //    this.today = 'Today';
+        //    this.clear = 'Clear';
+        //    this.close = 'Close';
+        //     var days = 15;
+        //    this.minDate = (new Date(this.currentTime.getTime() - (1000 * 60 * 60 * 24 * days))).toISOString();
+        //    this.maxDate = (new Date(this.currentTime.getTime() + (1000 * 60 * 60 * 24 * days))).toISOString();
+        //    this.onStart = function () {
+        //         console.log('onStart');
+        //     };
+        //    this.onRender = function () {
+        //         console.log('onRender');
+        //     };
+        //    this.onOpen = function () {
+        //         console.log('onOpen');
+        //     };
+        //    this.onClose = function () {
+        //         console.log('onClose');
+        //     };
+        //    this.onSet = function () {
+        //         console.log('onSet');
+        //     };
+        //    this.onStop = function () {
+        //         console.log('onStop');
+        //     };
 
     }]
 }
